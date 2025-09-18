1a4cde97147f2a84b661a48d72580bebd288f5f3
Lucas.S <lucassilvazfreire@gmail.com>
2025-09-18 17:56:20 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,21 @@
+num1 = float(input('digite um numero para somar:'))
+num2 = float(input('digite um numero para somar:'))
+soma = num1 + num2
+print ('resultado:', soma)
+print()
+num1 = float(input('digite um numero para subtrair:'))
+num2 = float(input('digite um numero para subtrair:'))
+sub = num1 - num2
+print('resultado:', sub)
+print()
+num1= float(input('digite um numero para multiplicar:'))
+num2= float(input('digite um numero para multiplicar:'))
+mut= num1 * num2
+print ('resultado:', mut)
+print()
+num1= float(input('digite um numero para dividir:'))
+num2= float(input('digite um numero para dividir:'))
+div= num1 / num2
+print ('resultado:', div)
+
+            
---
+++ aula3.py
@@ -0,0 +1,28 @@
+peso =  float(input('Peso: '))
+altura = float(input('altura: '))
+imc =  peso/altura ** 2
+print(imc)
+
+ano_atual = int(input('Ano atual: '))
+ano_nascimento = int(input('Ano de Nascimento: '))
+idade = ano_atual -  ano_nascimento
+print('Idade:', idade)
+
+
+# SINAIS ARITMÉTICOS
+
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+
+
+num1 = float(input('digite um numero'))
+num2 = float(input('digite um numero'))
+soma = num + num2
+print(soma)
+
+num1 = float(input('digite um numero'))
+num2 = float(input('digite um numero'))
+soma = num + num2
+print(soma)
