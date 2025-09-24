28f3b0a7838b4fce2e4f3126146b4335012584fc
Lucas.S <lucassilvazfreire@gmail.com>
2025-09-24 16:40:49 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,57 @@
+# Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+# Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+multi= (4*6)/2
+print('resultado',multi)
+
+# Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+# Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+# Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+
+media=(8+12+15)/3
+print('resultado',media)
+sub=(10-2)*5
+print('resultado',sub)
+div=(27/3)+5
+print('resultado',div)
+
+# Exercícios com variáveis / print() / input()(opcional):
+
+# Calcule o módulo ( % ) de 17 por 4.
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+
+módulo=(17%4)
+print('resultado',módulo)
+
+mult=(9*3)**2
+print('resultado',mult)
+
+# Calcule a raiz quadrada de 81.
+raiz=81**0.5
+print('resultado',raiz)
+
+# Adicione 20 a 3 vezes 4.
+adic=(3*4)+20
+print('resultado',adic)
+
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+mult=(15*2)-17
+print('resultado',mult)
+
+elev=5**5
+print('resultado',elev)
+
+# Calcule a média de 17, 21 e 25.
+media=(17+21+25)/3
+print('resultado',media)
+
+# Multiplique 11 por 2 e adicione 7.
+mult=(11*2)+7
+print('resultado',mult)
+
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+sub=(3*8-15)/2
+print('resultado',sub)
+
+# Eleve 2 à potência de 10.
+elev=2**10
+print('resultado',elev)
