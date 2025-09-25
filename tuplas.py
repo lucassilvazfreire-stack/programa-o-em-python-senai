d04b6ea987a9d98ddea05ea1d2cae9b51f2fa1f2
Lucas.S <lucassilvazfreire@gmail.com>
2025-09-25 17:10:27 -0300

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,85 @@
+# estutura de dados
+
+
+# variáveis -  mutável
+
+
+nome  =  'José'
+
+
+# listas 
+
+
+lista  =  [1,2,3]
+
+
+
+# tuplas - imutável + 
+
+
+# append
+# del
+# remove
+# pop
+# extend
+
+
+#  ok  +=()
+
+
+
+tupla = (10,20,20)
+print(tupla[0])
+
+
+tupla +=(120,30,30)
+documentos_cpfs  = (654654,46565,65454,5465465,5465456)
+
+
+# tupla.append(10)
+
+
+print(tupla)
+
+
+
+
+# dicionario chaves e valores 
+
+
+dicionario = {
+'chave':'valor',
+'chave':'valor',
+}
+
+
+
+pessoa = ['ana','bruna']
+idades = [10,30]
+
+
+pessoa = {
+'nome':'Laura',
+'idade':25,
+'endereco':'Rua 25',
+'livros':['x','y','z'],
+'dic':{
+'a':10,
+'b':30
+}
+}
+
+
+
+print(pessoa['livros'][2])
+
+
+
+
+# conjunto - dados únicos
+
+
+dados = {10,20,30,30,30,30}
+print(dados)
+
+
