87b6cb5927b8d8fa9a86e541bc0ebf713419d25d
Lucas.S <lucassilvazfreire@gmail.com>
2025-09-25 17:53:12 -0300

    Add files via upload

---
+++ atividade_dicionario.py
@@ -0,0 +1,48 @@
+# Crie um e-commerce com a estrutura de dicionários.
+
+
+# Produtos:  Livros, tablets e fones de ouvido
+
+
+e_commerce ={
+
+
+'Livros':{
+   'a':20.0,
+   'b':30.0,
+   'c':40},
+'tables':{
+'samsumng':100.0,
+'motorola':1000.0,
+},
+'fones':{
+'a':50,
+'b':60
+}   
+}
+
+
+
+e_commerce2={
+
+
+'table':20,
+'fone':10,
+'livro':100
+
+
+
+}
+
+
+
+e_commerce3 = {
+
+
+'fone':['a','b','c'],
+'valore_fones':[10,20,30],
+'tablets':['a','b','c'],
+'livro':['a','b','c'],
+'cpf':(123,456,78),
+'teste':{10,20,30}
+}
