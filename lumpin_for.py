69bdb7ffda214c9dfdba65a77e22623054853a38
Lucas.S <lucassilvazfreire@gmail.com>
2025-10-01 17:50:27 -0300

    Add files via upload

---
+++ lumpin_for.py
@@ -0,0 +1,16 @@
+# import random
+
+# for chances in range(3,0,-1):
+#    numro_aleatorio= random.randint(1,10)
+#    print('voçê tem apenas',chances,'chancês')
+#    chute= int(input('escolha um numero'))
+   
+#    if numro_aleatorio== chute:
+#      print('o nunero da maquina é',numro_aleatorio)
+#      print('ganhou o jogo!')
+#      break 
+  
+#    else:
+#       print('onumero da maquina é', numro_aleatorio)
+# else:
+#      print('voçê perdeu o jogo')
---
+++ lumpin_while.py
@@ -0,0 +1,12 @@
+# c = 0
+
+# while c <=999999999999999999999999999999:
+#     print(c)
+#     c = c +1
+l=[]
+lista_de_nomes=1
+while lista_de_nomes <=10:
+      nomes= input('digite nome:')
+      l.append(nomes)
+      print (l)
+      lista_de_nomes=lista_de_nomes+1
