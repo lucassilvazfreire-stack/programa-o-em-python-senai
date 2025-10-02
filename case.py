4539ac477d5645b8ddd7d2a394fb858cda28c0fb
Lucas.S <lucassilvazfreire@gmail.com>
2025-10-02 17:55:09 -0300

    Add files via upload

---
+++ case.py
@@ -0,0 +1,47 @@
+        #   |Verificando se o número é par ou ímpar|
+
+# numero = int(input('digite um numero '))
+
+# match numero:
+#     case numero if numero %2 == 0:
+#          print('numero par')
+#     case _:
+#         print('numro impar')     
+
+      
+
+
+        #  |Verificando se um número é positivo, negativo ou zero|
+
+# num= int(input('digite um numero'))
+# match num:
+#     case num if num >0:
+#         print ('positivo')
+#     case num if num <0:
+#         print('negativo')
+#     case _:
+#         print ('zero')
+
+
+
+    #    Verificando se uma string é vazia ou não
+
+# n = input('--')
+# match n:
+#     case n if n :
+#       print ('string vasia')
+#     case _:
+#       print ('string cheia')
+ 
+
+
+    #  Verificando se um número é maior, menor ou igual a 10
+
+n = int(input('numero'))
+match n:
+    case n if n >10:
+        print ('maior que 10')
+    case n if n <10:
+        print ('menor que 10')
+    case _:
+        print ('10')
